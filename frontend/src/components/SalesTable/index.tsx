5595825fb8e0a3146646353471578c04fd73203f
Guilherme <guilhermegsilverio@gmail.com>
2023-10-18 10:15:10 -0300

    SalesTable component

---
+++ frontend/src/components/SalesTable/index.tsx
@@ -0,0 +1,89 @@
+import './styles.css';
+
+function SalesTable() {
+  return (
+    <div className="base-card sales-table-container">
+      <h3 className="sales-table-title">SalesTable</h3>
+      <table className="sales-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Data</th>
+            <th>Genero</th>
+            <th>Categoria</th>
+            <th>Loja</th>
+            <th>Forma de pagamento</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/2020</td>
+            <td>Feminino</td>
+            <td>Roupas e Acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default SalesTable;
